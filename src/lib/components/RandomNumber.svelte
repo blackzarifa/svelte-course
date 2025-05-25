d09c326e2802a0461cb8d08a67c05bd44192be84
blackzarifa <rafaelirades@gmail.com>
2025-05-25 17:07:45 -0300

    full RandomNumber component

--- src/lib/components/RandomNumber.svelte
+++ src/lib/components/RandomNumber.svelte
@@ -4,6 +4,9 @@
 	let randomNumber = $state(Math.floor(Math.random() * 10));
 	let doubleRandomNumber = $derived(randomNumber * 2);
 
+	let history: number[] = $state([untrack(() => randomNumber)]);
+	let historyPTag: HTMLParagraphElement;
+
 	onMount(() => {
 		console.log('the component has mounted');
 		return () => {
@@ -14,7 +17,35 @@
 	onDestroy(() => {
 		console.log('the component is destroyed');
 	});
+
+	$effect.pre(() => {
+		console.log('$effect.pre', historyPTag?.innerText);
+		tick().then(() => {
+			console.log('After tick', historyPTag?.innerText);
+		});
+		return () => {
+			console.log('Pre Effect Cleanup');
+		};
+	});
+
+	$effect(() => {
+		console.log('$effect', historyPTag.innerText);
+		return () => {
+			console.log('Effect Cleanup');
+		};
+	});
 </script>
 
 <h2>The random number is: {randomNumber}</h2>
 <h2>Double random number is: {doubleRandomNumber}</h2>
+
+<p bind:this={historyPTag}>History: {history}</p>
+
+<button
+	onclick={() => {
+		randomNumber = Math.floor(Math.random() * 10);
+		history.push(randomNumber);
+	}}
+>
+	Generate
+</button>
