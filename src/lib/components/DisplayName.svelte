c23a59c9e7ddb80efd42b8552dccdb83b1b6f1b7
blackzarifa <rafaelirades@gmail.com>
2025-05-25 16:51:33 -0300

    display name component

---
+++ src/lib/components/DisplayName.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	let src = 'https://picsum.photos/250/300';
+	let bio = 'Hello, <b>this</b> is me.';
+
+	let username = $state('');
+	let firstName = $state('Ali');
+	let lastName = $state('Alaa');
+
+	let fullName = $derived.by(() => {
+		console.log('fullName Derived');
+		return `${firstName} ${lastName}`;
+	});
+
+	$effect(() => {
+		if (username || fullName) console.log('User has a username or a full name');
+	});
+</script>
+
+<button
+	onclick={() => {
+		console.log(fullName);
+	}}
+>
+	Get Full Name
+</button>
+
+<input bind:value={username} />
+<input
+	value={firstName}
+	oninput={(e) => {
+		firstName = e.currentTarget.value;
+		console.log(fullName);
+	}}
+/>
+<input bind:value={lastName} />
+
+<img {src} alt="A photo of {username || fullName}" />
+
+<h1>{username || fullName}</h1>
+<p>{@html bio}</p>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
