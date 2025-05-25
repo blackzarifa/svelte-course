a2f1657464163e1a702e3bdd27cd4218f8d080d6
blackzarifa <rafaelirades@gmail.com>
2025-05-25 16:56:15 -0300

    initializing RandomNumber component

---
+++ src/lib/components/RandomNumber.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { onDestroy, onMount, tick, untrack } from 'svelte';
+
+	let randomNumber = $state(Math.floor(Math.random() * 10));
+	let doubleRandomNumber = $derived(randomNumber * 2);
+
+	onMount(() => {
+		console.log('the component has mounted');
+		return () => {
+			console.log('the component has unmounted');
+		};
+	});
+
+	onDestroy(() => {
+		console.log('the component is destroyed');
+	});
+</script>
+
+<h2>The random number is: {randomNumber}</h2>
+<h2>Double random number is: {doubleRandomNumber}</h2>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import DisplayName from '$lib/components/DisplayName.svelte';
+	import RandomNumber from '$lib/components/RandomNumber.svelte';
 </script>
 
-<DisplayName />
+<RandomNumber />
