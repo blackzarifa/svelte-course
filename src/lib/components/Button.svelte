a2eedeb14c76326950db8959b154b10f3f4de0ca
blackzarifa <rafaelirades@gmail.com>
2025-05-26 19:20:37 -0300

    button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		left?: Snippet;
+		right?: Snippet;
+		children: Snippet;
+	}
+	let { left, right, children }: Props = $props();
+</script>
+
+<button>
+	{#if left}
+		<div class="left-content">
+			{@render left()}
+		</div>
+	{/if}
+	{@render children()}
+	{#if right}
+		<div class="right-content">
+			{@render right()}
+		</div>
+	{/if}
+</button>
+
+<style>
+	button {
+		border: none;
+		background-color: #ff3e00;
+		color: #ffffff;
+		padding: 0 20px;
+		height: 45px;
+		font-weight: bold;
+		border-radius: 5px;
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.left-content {
+		margin-inline-end: 10px;
+	}
+	.right-content {
+		margin-inline-start: 10px;
+	}
+</style>
