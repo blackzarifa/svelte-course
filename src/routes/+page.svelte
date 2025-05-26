e8b86126d880d8e811687b0c3fcb9d3d10e500f7
blackzarifa <rafaelirades@gmail.com>
2025-05-26 19:24:35 -0300

    add button to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { Search, AlarmCheck } from '@lucide/svelte';
+	import Button from '$lib/components/Button.svelte';
 	import Counter from '$lib/components/Counter.svelte';
 	import DisplayName from '$lib/components/DisplayName.svelte';
 	import RandomNumber from '$lib/components/RandomNumber.svelte';
@@ -10,3 +12,12 @@
 <hr />
 <Counter />
 <hr />
+<Button>
+	{#snippet left()}
+		<Search />
+	{/snippet}
+	Text
+	{#snippet right()}
+		<AlarmCheck />
+	{/snippet}
+</Button>
