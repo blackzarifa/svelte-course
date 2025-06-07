cbcd6821db6f675503806d4e74d8e600e4b83ae6
blackzarifa <rafaelirades@gmail.com>
2025-06-07 17:52:11 -0300

    starting sheet component

---
+++ src/lib/components/sheet/Sheet.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { alphabetToNumber, cellToIndex, numberToAlphabet } from './sheet-utils';
+
+	console.log(alphabetToNumber('AB'));
+	console.log(numberToAlphabet(27));
+	console.log(cellToIndex('Z12'));
+</script>
